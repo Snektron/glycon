123b1e1a5c6b729f1d95c3f17101bbbfa92c4ef0
Robin Voetter <robin@voetter.nl>
2021-12-29 01:39:49 +0100

    glydb: use editline for reading lines

--- glydb/src/interpreter.c
+++ glydb/src/interpreter.c
@@ -1,9 +1,11 @@
 #include "interpreter.h"
 #include "command/command.h"
 
-#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+
+#include <editline.h>
 
 static const char* prompt = "(glydb) ";
 
@@ -30,32 +32,14 @@ void interpreter_do_line(struct interpreter* interp, size_t len, const char line
 void interpreter_repl(struct interpreter* interp) {
     interp->quit = false;
 
-    char* line = NULL;
-    size_t buflen = 0;
-
     while (!interp->quit) {
-        printf("%s", prompt);
-        ssize_t len = getline(&line, &buflen, stdin);
-        if (len >= 0) {
-            if (len > 0 && line[len - 1] == '\n') {
-                line[len - 1] = 0;
-                interpreter_do_line(interp, (size_t) len - 1, line);
-            } else {
-                // User didn't end prompt with a newline - add one for them
-                puts("");
-                interpreter_do_line(interp, (size_t) len, line);
-            }
-        } else if (errno == 0) {
-            // End of input, just shut down.
-            // Still print an end-of-line though
-            puts("");
-            goto exit;
+        char* line = readline(prompt);
+        if (!line) {
+            return;
         } else {
-            perror("error");
-            goto exit;
+            size_t len = strlen(line);
+            interpreter_do_line(interp, len, line);
+            free(line);
         }
     }
-
-exit:
-    free(line);
 }
